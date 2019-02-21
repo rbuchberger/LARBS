6993f9ccd2c0a1849135a0c8a67e0164e0670c35
Robert Buchberger <rbuchberger@gmail.com>
2019-02-21 15:01:49 +0100

    customized for me

--- larbs.sh
+++ larbs.sh
@@ -14,8 +14,8 @@ while getopts ":a:r:p:h" o; do case "${o}" in
 esac done
 
 # DEFAULTS:
-[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
+[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/rbuchberger/dotfiles"
+[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/rbuchberger/LARBS/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 
 ### FUNCTIONS ###
